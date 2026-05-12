0ec6a76f6b7527ad96bc1b278294787b89318a1c
Benoit Parmentier <benoit.parmentier@gmail.com>
2026-05-11 20:43:05 -0400

    fix call to uri and bucket for mlflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 
 exec uv run python train.py \
     --tracking_uri "${TRACKING_URI:-file:./mlruns}" \
-    --artifact_location "${ARTIFACT_LOCATION:-gs://my-bucket/mlflow/artifacts}" \
+    ${ARTIFACT_LOCATION:+--artifact_location "$ARTIFACT_LOCATION"} \
     "$@"
